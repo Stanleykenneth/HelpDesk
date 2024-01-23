ef05772501ddd54289a53980692eaa0c0dada1a3
Kenneth Stanley <kstanley_bass@hotmail.com>
2024-01-23 16:46:13 -0300

    [Domain] Anotações
    Anotações  JPA para classes de domínio

--- src/main/java/com/kenne/helpdesk/domain/Tecnico.java
+++ src/main/java/com/kenne/helpdesk/domain/Tecnico.java
@@ -3,16 +3,27 @@ package com.kenne.helpdesk.domain;
 import java.util.ArrayList;
 import java.util.List;
 
+import com.kenne.helpdesk.domain.enums.Perfil;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.OneToMany;
+
+@Entity
 public class Tecnico extends Pessoa{
 
+	private static final long serialVersionUID = 1L;
+	
+	@OneToMany(mappedBy = "tecnico")
 	private List<Chamado> chamados = new ArrayList<>();
 
 	public Tecnico() {
 		super();
+		addPerfis(Perfil.TECNICO);
 	}
 
 	public Tecnico(Integer id, String nome, String cpf, String email, String senha) {
 		super(id, nome, cpf, email, senha);
+		addPerfis(Perfil.TECNICO);
 	}
 
 	public List<Chamado> getChamados() {
